6569b4199606ea5eb3a14d5a7da8a74cc8684d07
fabiopanassol <142537493+fabiopanassol@users.noreply.github.com>
2023-08-21 21:41:48 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+/*
+* Fábio Panassol
+* Primeiro Comentario
+*/
+console.log( "Olá mundo!");
+//prompt ("Digite seu nome");
+
+//Variaveis
+const variavelA = 100;
+const variavelB = variavelA + 50;
+console.log( variavelB );
+
+let numero = 50;
+console.log(numero);
+numero = 80;
+console.log(numero);
+
+const nome = "Fábio"
+const idade = 19
+
+console.log("Olá!", "Meu nome é", nome, "e eu tenho", idade, "anos");
+
+const nomeExercicio = "Fábio"
+const sobrenome = "Panassol"
+const idadeExercicio = 19
+const estudante = "eu sou estudante"
+console.log("Oi,", "eu sou o", nomeExercicio, sobrenome, ", eu tenho", idade, "e", estudante);
